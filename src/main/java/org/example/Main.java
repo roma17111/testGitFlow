eac411893fe8c4f23e02579acb3b573956293bc1
roman <romanze1706@gmail.com>
2023-02-27 12:10:39 +0300

    test4r

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,5 +5,6 @@ public class Main {
         System.out.println("Hello world!!");
         System.out.println("test");
         System.out.println("test1");
+        System.out.println("test2");
     }
 }
