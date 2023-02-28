bd70fc76308e94eec7caa863fde1431e9ae55e19
Verkin78 <otrajenie.jc@gmail.com>
2023-02-28 15:19:38 +0300

    zzzzzzzzzzzqqqqqqq

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -10,7 +10,6 @@ public class Main {
         System.out.println("test2");
         System.out.println("test2");
         System.out.println("test2");
-        System.out.println("test2");
-        System.out.println("test2");
+
     }
 }
