5d0f15b96ed92e565f233f2e1484ea890f888c15
Verkin78 <otrajenie.jc@gmail.com>
2023-02-28 15:24:53 +0300

    zzzzzzzzzzzqqqqqqq

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -9,7 +9,7 @@ public class Main {
         System.out.println("test2");
         System.out.println("test2");
         System.out.println("test2");
-        System.out.println("test2");
+        System.out.println("test2rererer");
 
     }
 }
